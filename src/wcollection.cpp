fac5f1215505728b7a8ac62c3554c8491cc336c6
MrMoonshine <mrmoonshine.goo@gmail.com>
2021-05-23 11:38:26 +0200

    merge readme to pi project

--- src/wcollection.cpp
+++ src/wcollection.cpp
@@ -4,5 +4,5 @@ Wcollection::Wcollection(QWidget *parent) : QToolBox(parent){
   addItem(new ExampleButton(this),"Push-Button");
   addItem(new CheckBoxes(this),"Checkboxes");
   addItem(new Lcd(this),"Displays W.I.P");
-  addItem(new QLabel("Oida"),"Oida");
+  addItem(new QLabel("Server"),"Server");
 }
